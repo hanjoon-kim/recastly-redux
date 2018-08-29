c24674874f9bcda98976c9bc4e00aaadaf426a1e
Hanjoon Kim <hanjoon3@gmail.com>
2018-08-28 20:04:13 -0700

    Implement actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,11 +2,20 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
+import fetch from 'whatwg-fetch';
 
-
-var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
+var handleVideoSearch = q => {
+  return fetch('https://www.googleapis.com/youtube/v3/search', {
+    q: q,
+    key: YOUTUBE_API_KEY,
+    maxResults: 5,
+    videoEmbeddable: true,
+    part: 'snippet',
+    type: 'video'
+  })
+    .then(response => response.json())
+    .then(videos => dispatch(changeVideoList(videos)))
+    .catch(console.log('error with search'));
 };
 
 export default handleVideoSearch;
